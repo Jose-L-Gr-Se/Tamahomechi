e7aeb559490ee8afeea908a8bebaad69a58ded09
Jose-L-Gr-Se <josegrande051292@gmail.com>
2026-04-14 11:22:29 +0200

    fix: type cookiesToSet in supabase server

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,5 +1,6 @@
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
+import { type ResponseCookie } from "next/dist/compiled/@edge-runtime/cookies";
 
 export async function createServerSupabaseClient() {
   const cookieStore = await cookies();
@@ -12,7 +13,7 @@ export async function createServerSupabaseClient() {
         getAll() {
           return cookieStore.getAll();
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: { name: string; value: string; options?: ResponseCookie }[]) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options)
